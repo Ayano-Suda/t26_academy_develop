b229e7b6ed81e95250585197a1c2024c9d909f10
MT 須田 彩乃 <ayano.suda@metateam.co.jp>
2026-05-11 20:40:16 +0900

    書籍登録_4

--- src/main/java/jp/co/metateam/library/service/BookMstService.java
+++ src/main/java/jp/co/metateam/library/service/BookMstService.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import io.micrometer.common.util.StringUtils;
+import jp.co.metateam.library.model.Account;
 import jp.co.metateam.library.model.BookMst;
 import jp.co.metateam.library.model.BookMstDto;
 import jp.co.metateam.library.repository.BookMstRepository;
@@ -29,6 +30,8 @@ public class BookMstService {
         List<BookMst> books = this.bookMstRepository.findLimitedBook();
         List<BookMstDto> bookMstDtoList = new ArrayList<BookMstDto>();
 
+        // 書籍の在庫数を取得
+        // FIXME: 現状は書籍ID毎にDBに問い合わせている。一度のSQLで完了させたい。
         for (int i = 0; i < books.size(); i++) {
             BookMst book = books.get(i);
             BookMstDto bookMstDto = new BookMstDto();
@@ -37,7 +40,20 @@ public class BookMstService {
             bookMstDto.setTitle(book.getTitle());
             bookMstDtoList.add(bookMstDto);
         }
+
         return bookMstDtoList;
-        //あああああ
+    }
+
+    // 以下入力
+    @Transactional//この処理を1セットのDB処理として扱う。途中でエラーなら保存をすべてなかったことに
+    public void save(BookMstDto bookMstDto) { //puplicでコントローラーから呼べる。voidで戻り値なし、今回は保存のみなので画面へ帰す値は不要
+//saveは保存処理メソッド
+        BookMst book = new BookMst();//Controllerから受け取った画面入力データ（titleとisbn）
+       
+
+        book.setTitle(bookMstDto.getTitle());//画面入力をDB保存用Entityへ詰め替える
+        book.setIsbn(bookMstDto.getIsbn());//画面入力をDB保存用Entityへ詰め替える
+
+        this.bookMstRepository.save(book);//Repositoryへ保存依頼(JpaRepository継承)
     }
 }
